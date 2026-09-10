a0986e4b9b5cdf2d8c7ae1bd6931433631d4fa28
Gabriel Fonseca <gabrielfonseca555@gmail.com>
2026-09-10 02:10:17 -0300

    Create

---
+++ src/test/java/com/leirose/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leirose.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
